ecc13601c276f3ce1166e0a63ed688aeb76be7b1
TennisMateApp <support@tennis-mate.com.au>
2025-07-12 22:39:06 +1000

    Fix withAuth typing issue for Vercel build

--- components/withAuth.tsx
+++ components/withAuth.tsx
@@ -4,11 +4,10 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "@/lib/firebase";
+import type { ComponentType } from "react";
 
-export default function withAuth<T extends Record<string, any>>(
-  WrappedComponent: React.ComponentType<T>
-) {
-  return function ProtectedComponent(props: T) {
+export default function withAuth<T extends object>(WrappedComponent: ComponentType<T>) {
+  const ProtectedComponent = (props: T) => {
     const router = useRouter();
     const [loading, setLoading] = useState(true);
 
@@ -30,4 +29,6 @@ export default function withAuth<T extends Record<string, any>>(
 
     return <WrappedComponent {...props} />;
   };
+
+  return ProtectedComponent;
 }
