a979bc7687afd04c5bd480d2af6efc8ee1ec4464
TennisMateApp <support@tennis-mate.com.au>
2025-11-26 21:19:43 +1100

    Revert

--- next.config.js
+++ next.config.js
@@ -3,21 +3,14 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // disables PWA in dev
+  disable: process.env.NODE_ENV === "development",
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = withPWA({
   reactStrictMode: true,
-
-  // For static export + Capacitor
-  output: "export",
-
   images: {
     domains: ["firebasestorage.googleapis.com"],
-    // Required for static export so Next.js doesn't rely on the image optimizer
-    unoptimized: true,
   },
-};
-
-module.exports = withPWA(nextConfig);
+  // ❌ DO NOT add: output: 'export'
+  // ❌ DO NOT add: distDir: 'out'
+});
