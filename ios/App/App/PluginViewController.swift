96ab323cbc1b4e6666332e58eba361c64b3bb706
William Bourke <billyray@Williams-Mac-mini.local>
2025-11-29 20:24:49 +1100

    Update IOS export compliance Info.plist

--- ios/App/App/PluginViewController.swift
+++ ios/App/App/PluginViewController.swift
@@ -1,29 +1,49 @@
-//
-//  PluginViewController.swift
-//  App
-//
-//  Created by William Bourke on 9/11/2025.
-//
-
+// PluginViewController.swift
 import UIKit
+import Capacitor
 
-class PluginViewController: UIViewController {
-
-    override func viewDidLoad() {
+class PluginViewController: CAPBridgeViewController {
+    override open func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        DispatchQueue.main.async {
+            self.setupWebViewPadding()
+        }
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override open func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        DispatchQueue.main.async {
+            self.setupWebViewPadding()
+        }
     }
-    */
 
+    override open func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setupWebViewPadding()
+    }
+
+    private func setupWebViewPadding() {
+        guard let webView = self.webView else { return }
+
+        var top: CGFloat = 0
+        var bottom: CGFloat = 0
+        var left: CGFloat = 0
+        var right: CGFloat = 0
+
+        if #available(iOS 13.0, *) {
+            let window = view.window ?? UIApplication.shared.windows.first { $0.isKeyWindow }
+            top = window?.safeAreaInsets.top ?? 0
+            bottom = window?.safeAreaInsets.bottom ?? 0
+            left = window?.safeAreaInsets.left ?? 0
+            right = window?.safeAreaInsets.right ?? 0
+        } else {
+            top = UIApplication.shared.statusBarFrame.height
+        }
+
+        webView.frame.origin = CGPoint(x: left, y: top)
+        webView.frame.size = CGSize(
+            width: UIScreen.main.bounds.width - left - right,
+            height: UIScreen.main.bounds.height - top - bottom
+        )
+    }
 }
