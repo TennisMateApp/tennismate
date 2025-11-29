447b2917a4442afc1411e020bf08894d112b7a2a
William Bourke <billyray@Williams-Mac-mini.local>
2025-11-29 20:05:52 +1100

    set ITSAppUsesNonExemptEncyption to NO

---
+++ ios/App/App/PluginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PluginViewController.swift
+//  App
+//
+//  Created by William Bourke on 9/11/2025.
+//
+
+import UIKit
+
+class PluginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
