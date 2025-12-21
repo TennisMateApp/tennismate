a1f1e7af805544e021a73f82c7a95f3e29466301
TennisMateApp <support@tennis-mate.com.au>
2025-12-21 17:23:28 +1100

    Fix Cloud Functions region mismatch (southeast2)

--- functions/lib/callables.ts
+++ functions/lib/callables.ts
@@ -1,7 +1,7 @@
 import { getFunctions, httpsCallable } from "firebase/functions";
 import { app } from "@/lib/firebase"; // your initialized client app
 
-const functions = getFunctions(app, "australia-southeast1");
+const functions = getFunctions(app, "australia-southeast2");
 
 export const cfProposeEvent = httpsCallable(functions, "proposeEvent");
 export const cfUpdateEvent  = httpsCallable(functions, "updateEvent");
