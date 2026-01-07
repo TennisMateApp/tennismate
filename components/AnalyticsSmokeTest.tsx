d6ab10ba187fa100221d21ed5c0d66e915648404
TennisMateApp <support@tennis-mate.com.au>
2026-01-07 16:58:10 +1100

    analytics issue

--- components/AnalyticsSmokeTest.tsx
+++ components/AnalyticsSmokeTest.tsx
@@ -1,21 +1,17 @@
 "use client";
 
 import { useEffect } from "react";
-import { analytics } from "@/lib/firebaseConfig";
+import { getAnalyticsIfSupported } from "@/lib/firebaseConfig";
 import { logEvent } from "firebase/analytics";
 
 export default function AnalyticsSmokeTest() {
   useEffect(() => {
-    if (!analytics) {
-      console.log("[GA] analytics not ready (null) - skipping test event");
-      return;
-    }
+    (async () => {
+      const analytics = await getAnalyticsIfSupported();
+      if (!analytics) return;
 
-    console.log("[GA] logging test event: tennismate_analytics_test");
-    logEvent(analytics, "tennismate_analytics_test", {
-      source: "web",
-      ts: Date.now(),
-    });
+      logEvent(analytics, "analytics_smoke_test");
+    })();
   }, []);
 
   return null;
